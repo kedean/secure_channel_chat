57c974651d4aa8ab72fd0838d78e5fc9e31133f4
Kevin Dean <kevindean@KevinLaptop.local>
2013-02-13 19:39:17 -0600

    Secure channel partially complete, added key agreement and shared-key encryption, still needs authenticators

--- channel.py
+++ channel.py
@@ -1,6 +1,15 @@
 import socket
 import sys
 
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Random import random
+from Crypto.Cipher import AES
+from Crypto.Util import Counter
+import pickle
+
+import numpy
+
 class Channel:
     socket = None
     address = None
@@ -8,6 +17,16 @@ class Channel:
     connection, client_address = None, None
     buffer_size = 4096
     connection_type = None
+    
+    _rsa_key = None
+    _rsa_keylength = 2048
+    _rsa_other_pubkey = None
+    _rsa_cipher = None
+    _rsa_othercipher = None
+    
+    _shared_key = None
+    _encrypt_cipher = None
+    _decrypt_cipher = None
     
     def __init__(self, address, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,21 +46,104 @@ class Channel:
         if self.connection is None:
             return ("No connection.", -2)
         try:
+            if self._encrypt_cipher is not None:
+                message = pickle.dumps(self._encrypt_cipher.encrypt(message))
+                
             self.connection.send(message)
             return ("Success.", 0)
         except Exception:
             return ("An exception occurred in transport.", -1)
+        
     def receiveMessage(self):
         if self.connection is None or self.connection_type is None:
             return ("No connection exists.", -3)
         try:
             data = self.connection.recv(self.buffer_size)
             if data:
+                if self._decrypt_cipher is not None:
+                    data = self._decrypt_cipher.decrypt(pickle.loads(data))
                 return (data, 0)
             else:
                 return ("No connection.", -1)
         except Exception:
             return ("No data recieved.", -2)
+    
+    def _int_to_bytes(self, val, num_bytes):
+        return bytearray([(val & (0xff << pos*8)) >> pos*8 for pos in range(num_bytes)])
+    
+    def startHandshake(self):
+        self._rsa_key = RSA.generate(self._rsa_keylength)
+        self._rsa_cipher = PKCS1_OAEP.new(self._rsa_key)
+        pem = self._rsa_key.exportKey()
+        self.sendMessage(pem)
+        while self._rsa_other_pubkey is None:
+            result, error = self.receiveMessage()
+            
+            if error == -1:
+                return -1
+            elif error == 0: #response
+                self._rsa_other_pubkey = RSA.importKey(result)
+                self._rsa_othercipher = PKCS1_OAEP.new(self._rsa_other_pubkey)
+        
+        exp1 = random.getrandbits(256)
+        
+        msg_crypt = self._rsa_othercipher.encrypt(str(exp1))
+        self.sendMessage(pickle.dumps(msg_crypt))
+        
+        exp2 = None
+        while exp2 is None:
+            result, error = self.receiveMessage()
+            
+            if error == -1:
+                return -1
+            elif error == 0: #response
+                msg = pickle.loads(result)
+                try:
+                    exp2 = long(self._rsa_cipher.decrypt(msg))
+                except Exception as e:
+                    exit("could not decrypt")
+        
+        self._shared_key = buffer(self._int_to_bytes(exp1 ^ exp2, 256 / 8))
+        
+    
+    def acceptHandshake(self):
+        while self._rsa_other_pubkey is None:
+            result, error = self.receiveMessage()
+            
+            if error == -1:
+                return -1
+            elif error == 0: #response
+                self._rsa_other_pubkey = RSA.importKey(result)
+                self._rsa_othercipher = PKCS1_OAEP.new(self._rsa_other_pubkey)
+        
+        self._rsa_key = RSA.generate(self._rsa_keylength)
+        self._rsa_cipher = PKCS1_OAEP.new(self._rsa_key)
+        self.sendMessage(self._rsa_key.exportKey())
+        
+        exp1 = None
+        while exp1 is None:
+            result, error = self.receiveMessage()
+            
+            if error == -1:
+                return -1
+            elif error == 0: #response
+                msg = pickle.loads(result)
+                try:
+                    exp1 = long(self._rsa_cipher.decrypt(msg))
+                except Exception as e:
+                    exit("could not decrypt")
+        
+        exp2 = random.getrandbits(256)
+        msg_crypt = self._rsa_othercipher.encrypt(str(exp2))
+        self.sendMessage(pickle.dumps(msg_crypt))
+        
+        self._shared_key = buffer(self._int_to_bytes(exp1 ^ exp2, 256 / 8))
+        
+    def initializeSecureChannel(self):
+        ctr = Counter.new(128)
+        self._encrypt_cipher = AES.new(self._shared_key, AES.MODE_CTR, counter=ctr)
+        self._decrypt_cipher = AES.new(self._shared_key, AES.MODE_CTR, counter=ctr)
+        
 
 class Listener(Channel):
     
@@ -62,6 +164,10 @@ class Listener(Channel):
                         yield ("No connection made.", -1)
                 self.connection.setblocking(0)
                 self.connection_type = "server"
+                
+                self.startHandshake()
+                self.initializeSecureChannel()
+                
                 yield ("Success.", 0)
             except socket.error:
                 yield ("Address is already in use or port is unusable.", -3) #make more informative
@@ -79,6 +185,10 @@ class Client(Channel):
             self.connection = self.socket
             self.connection.setblocking(0)
             self.connection_type = "client"
+            
+            self.acceptHandshake()
+            self.initializeSecureChannel()
+            
             return ("Success.", 0)
         except socket.error:
             return ("The connection was refused or failed!", -3) #make more informative
